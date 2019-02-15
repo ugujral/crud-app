2025c5b921b2060481496cea0d2727933675c733
Uttej Gujral <uttej@uttejs-mbp.attlocal.net>
2019-02-14 23:48:14 -0800

    Added recipes and trying to compare items to recipe ingredients to return the recipe to user

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ const itemsList = document.querySelector('.grocery');
 const removeAllItems = document.querySelector('.removeItems');
 const recipeGenerator = document.querySelector('.recipe');
 let items = [];
+let recipes = [
+  {
+    name: 'pasta',
+    ingredients: ['pasta', 'sauce', 'shrimp', 'squash'],
+    recipe: ['place in boiling water', 'stir until soft', 'drain out', 'add sauce to pasta and mix', 'add in desired meat or vegetables'] 
+  },
+  {
+    name: 'pizza',
+    ingredients: ['dough', 'tomato sauce', 'cheese', 'chicken', 'onion', 'pepperoni'],
+    recipe: ['take dough and knead dough into circle', 'apply sauce', 'place toppings', 'cook until lightly brown on the bottom', 'EAT!!']
+  }
+];
+localStorage.setItem('recipes', JSON.stringify(recipes))
 
 //Function that adds groceries to items array above
 function addItem(e) {
@@ -14,13 +27,13 @@ function addItem(e) {
   };
 
   items.push(item);
-  pushtoGroceryTag(items, itemsList); //Runs the function to take items and push them into items array
+  pushToGroceryTag(items, itemsList); //Runs the function to take items and push them into items array
   localStorage.setItem('items', JSON.stringify(items)); //JSON.stringify to take the input object into a string since localStorage is a set of keys and values 
   this.reset(); //Empties input box so user can add new items
 };
 
 //Takes items passed and places into a html list 
-function pushtoGroceryTag(grocery = [], groceryList) { // Important to have empty array to loop over nothing if nothing is passed
+function pushToGroceryTag(grocery = [], groceryList) { // Important to have empty array to loop over nothing if nothing is passed
   groceryList.innerHTML = grocery.map((grocery, i) => {
     return `
       <li>
@@ -38,7 +51,20 @@ function removeAll() {
   document.location.reload()
 };
 
+function recipeMaker() {
+  let recipeArray = Object.assign([], recipes);
+  let itemsArray = Object.assign([], items);
+  for (var i = 0; i < itemsArray.length; i++) {
+    for (var j = 0; j < recipeArray.length; j++) {
+      if (JSON.stringify(itemsArray[i].text) === JSON.stringify(recipeArray[i].ingredients)) {
+        return 'found';
+      }
+    }
+  }
+};
+
 addItems.addEventListener('submit', addItem);
 removeAllItems.addEventListener('click', removeAll);
+recipeGenerator.addEventListener('click', recipeMaker);
 
-pushtoGroceryTag(items, itemsList);
+pushToGroceryTag(items, itemsList);
